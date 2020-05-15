9e75339ca27789f66e1edfe09d0c37d109bfac70
Erhan Yakut <yakuter@gmail.com>
2020-05-15 18:58:21 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Menu from './components/Menu';
 import Logo from './components/Logo';
+import Download from './components/Download';
 import './App.css';
 
 
@@ -16,9 +17,11 @@ function App() {
         <div className="Logo">
           <Logo />
         </div>
+        <div className="Download">
+          <Download />
+        </div>
       </div>
       <div className="Right">,
-        dsfaf
       </div>
     </div>
   );
---
+++ src/components/Download.js
@@ -0,0 +1,12 @@
+import React from 'react';
+ 
+function Download() {
+    
+    return (
+        <button
+          className="btn btn-default"
+          >MacOS</button>
+      );
+}
+ 
+export default Download;
