cd56246e895e0d71dc21f4e66119be67a982dafb
Erhan Yakut <yakuter@gmail.com>
2020-05-24 23:33:46 +0300

    update for remote

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: `http://localhost:3625`,
+  // baseURL: `http://localhost:3625`,
+  baseURL: `http://89.252.131.83:3625`,
   responseType: "json"
 });
