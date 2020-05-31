d5cb0cb24a350452bb1f785730a3b2c014cea5e0
Erhan Yakut <yakuter@gmail.com>
2020-06-01 02:04:36 +0300

    update keep in mind

--- src/components/KeepInMind.js
+++ src/components/KeepInMind.js
@@ -4,7 +4,7 @@ function KeepInMind() {
     return (
         <div>
             <h3>Keep in mind</h3>
-            <p>We do not store your master password anywhere and if you forget your master password there is no way to restore it.</p>
+            <p>We do not store your master password anywhere. If you forget your master password there is no way to restore it.</p>
         </div>
     );
 }
