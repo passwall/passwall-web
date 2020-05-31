d4d0962d16e06ce98832a1faa9d95a9d1f463f4e
Erhan Yakut <yakuter@gmail.com>
2020-05-31 16:14:40 +0300

    update contact link

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Menu() {
     const links = Array (
         {name: "Give Feedback", url: "https://passwall.typeform.com/to/GAv1h2"},
-        {name: "Contact", url: "mailto:hello@passwall.io"},
+        {name: "Contact", url: "https://passwall.typeform.com/to/b2un6b"},
         {name: "Community", url: "https://passwall.slack.com/"},
         {name: "Twitter", url: "https://twitter.com/pass_wall"},
     );
