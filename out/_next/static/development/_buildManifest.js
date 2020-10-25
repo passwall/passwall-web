fe2a7383795112299cf6334f142b282ecfcc6dbe
Erhan Yakut <yakuter@gmail.com>
2020-10-25 15:49:48 +0300

    update yarn command

--- out/_next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/free":["static\u002Fchunks\u002Fpages\u002Ffree.js"],"/thankyou":["static\u002Fchunks\u002Fpages\u002Fthankyou.js"],sortedPages:["\u002F_app","\u002F_error","\u002Ffree","\u002Fthankyou"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
