467da5e7c29c8856031cdb92b5737f90b1456d1c
bufgix <ooruc471@yandex.com>
2021-03-18 20:46:12 +0300

    feat: add confirmation page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react'
 import Head from 'next/head'
-import StoreContext from '../store'
 
 import Layout from '../components/layout'
 import LeftSide from '../components/leftside'
@@ -9,8 +8,6 @@ import Text from '../components/text'
 import Card, { FreeCard, PaidCard } from '../components/card'
 
 function HomePage() {
-  const store = useContext(StoreContext)
-
   return (
     <Layout>
       <Head>
