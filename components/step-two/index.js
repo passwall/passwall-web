dd457b49feda0667bc7735dbf4b04ddbb7966820
Erhan Yakut <yakuter@gmail.com>
2021-03-22 11:52:52 +0300

    add new logic (#43)
    * updated workflow
    
    * update workflow
    
    * update yarn command
    
    * update yarn command
    
    * updated links
    
    * docs: add storybook
    
    * chore: add favicons
    
    * Add Favicons and storybook docs (#19)
    
    Closes #14 
    
    * docs: add storybook
    * chore: add favicons
    
    * updated webflow
    
    * added captcha key
    
    * fix: change contants
    
    * Fix download constants (#22)
    
    * docs: add storybook
    * chore: add favicons
    * Add Recaptcha and bugfixes (#21)
    * Add Favicons and storybook docs (#19)
    * Added captcha key
    * Bugfixes
    * fix: change contants
    
    * removed api key
    
    * Add google recaptcha state to signup value
    
    * remove console log
    
    * update api url
    
    * fix: add reset to recaptcha
    
    * recaptcha reset  (#27)
    
    * fix: change contants
    * fix: add reset to recaptcha
    
    * fix: google recaptcha change
    
    * recaptcha (#28)
    
    * fix: google recaptcha
    
    Co-authored-by: Erhan Yakut <yakuter@gmail.com>
    
    * updated recaptcha
    
    * update api url
    
    * updated download urls
    
    * refactor: add more validation
    
    * validation (#31)
    
    * Add validation to name field
    
    * update actions
    
    * Updated error message of Name input on Sign-up form. (#34)
    
    * Background & Button edit. (#33)
    
    Background sıkışması düzeltildi. Butonların hover durumları yapıldı. Css Button extends kısmından kurtarıldı.
    
    * update constants
    
    * update download links
    
    * Bump ini from 1.3.5 to 1.3.8 (#38)
    
    Bumps [ini](https://github.com/isaacs/ini) from 1.3.5 to 1.3.8.
    - [Release notes](https://github.com/isaacs/ini/releases)
    - [Commits](https://github.com/isaacs/ini/compare/v1.3.5...v1.3.8)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump axios from 0.20.0 to 0.21.1 (#37)
    
    Bumps [axios](https://github.com/axios/axios) from 0.20.0 to 0.21.1.
    - [Release notes](https://github.com/axios/axios/releases)
    - [Changelog](https://github.com/axios/axios/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/axios/axios/compare/v0.20.0...v0.21.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump elliptic from 6.5.3 to 6.5.4
    
    Bumps [elliptic](https://github.com/indutny/elliptic) from 6.5.3 to 6.5.4.
    - [Release notes](https://github.com/indutny/elliptic/releases)
    - [Commits](https://github.com/indutny/elliptic/compare/v6.5.3...v6.5.4)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * fix: change back button behaviour
    
    * feat: add confirmation page
    
    * build: add export scirpt
    
    * add extension urls
    
    * add upgrade page
    
    * new login (#42)
    
    Co-authored-by: bufgix <ooruc471@yandex.com>
    Co-authored-by: Oguzhan INAN <oguzhan3488@gmail.com>
    Co-authored-by: İzni Burak Demirtaş <izniburak@outlook.com>
    Co-authored-by: Hüseyin Durmuş <kirpiart@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ components/step-two/index.js
@@ -0,0 +1,140 @@
+import React, { useState, useRef } from 'react'
+import { useRouter } from 'next/router'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { ToastContainer, toast } from 'react-toastify'
+import * as yup from 'yup'
+import cn from 'classnames'
+import styles from './index.module.scss'
+import * as Icons from 'heroicons-react'
+
+import Text from '../text'
+import Button from '../button'
+import Api from '../../api'
+
+function ErrorMsg({ messages = [] }) {
+  return (
+    <div className={styles.error}>
+      <Text className={styles.errorHead} tag="h5" theme="regular">
+        Oops!
+      </Text>
+      {messages && (
+        <ul>
+          {messages.map((item, index) => (
+            <li key={index}>
+              <Text tag="p" theme="small">
+                {`* ${item}`}
+              </Text>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  )
+}
+
+export const FORM_TYPES = {
+  FREE: 'pro',
+  PRO: 'free'
+}
+
+const schema = yup.object().shape({
+  code: yup
+    .string()
+    .matches(/^\d+$/, {
+      message: 'Code can only contain numbers'
+    })
+    .required()
+})
+
+export function TextInput({
+  label,
+  name,
+  register,
+  className,
+  placeholder,
+  errors,
+  type = 'text',
+  ...props
+}) {
+  return (
+    <div className={cn(styles.input, className)}>
+      <label htmlFor={name}>{label}</label>
+      <input
+        type="text"
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        ref={register}
+        className={cn({ error: errors })}
+        type={type}
+        {...props}
+      />
+      {errors && (
+        <Text tag="span" theme="small">
+          {errors.message}
+        </Text>
+      )}
+    </div>
+  )
+}
+
+export default function Form() {
+  const router = useRouter()
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema)
+  })
+  let formType = localStorage.getItem("formType")
+  
+  const verifyCodeAPI = ({ code, email }) => {
+    return Api.get(`/auth/verify/`+code+`?email=`+email)
+      .then((data) => Promise.resolve(data))
+      .catch((err) => {
+        if (err.response.status === 400) {
+          console.log(err.response.data.errors)
+          toast(<ErrorMsg messages={err.response.data.errors} />)
+        } else {
+          console.error(err)
+          toast(<ErrorMsg messages={['Server Error']} />)
+        }
+        return Promise.reject(err)
+      })
+  }
+
+  const onSubmit = async ({ code }) => {
+    let email = localStorage.getItem("email")
+    verifyCodeAPI({ code, email })
+      .then(() => {
+        router.push('/step-three')
+      })
+      .catch((err) => console.error(err))
+  }
+
+  return (
+    <>
+      <ToastContainer hideProgressBar />
+      <form
+        className={styles.Form}
+        onSubmit={handleSubmit(onSubmit)}
+        method="POST"
+      >
+        <Icons.ArrowLeft onClick={() => router.push('/'+formType)} />
+        <Text tag="h3" theme="heromd" fancy={formType === FORM_TYPES.PRO}>
+          {'Enter your code'}
+        </Text>
+        <TextInput
+          label="Please enter the verification code at your e-mail inbox."
+          name="code"
+          placeholder="123456"
+          register={register()}
+          errors={errors.code}
+        />
+        <Button type="submit" value="Submit">
+          <Text tag="p" theme="regular" className={styles.btn}>
+            {'Continue to Final Step'}
+          </Text>
+        </Button>
+      </form>
+    </>
+  )
+}
